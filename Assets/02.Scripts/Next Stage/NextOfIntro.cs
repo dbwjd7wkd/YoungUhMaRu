b8edcf2951f2ef45bc61ff3db039194b30f70f4d
YuJeong Jeong <dbwjd7wkd@naver.com>
2020-03-25 20:17:41 +0900

    다음 스테이지 포탈 자동 생성

--- Assets/02.Scripts/Next Stage/NextOfIntro.cs
+++ Assets/02.Scripts/Next Stage/NextOfIntro.cs
@@ -3,47 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextOfIntro : StateMachineBehaviour
+public class NextOfIntro : MonoBehaviour
 {
-    public string Scene;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    Animator ani;
+    // Start is called before the first frame update
+    void Start()
     {
-        if(Scene.Length > 0)
-        {
-            SceneManager.LoadScene(Scene);
-        }
-        else
-        {
-            SceneManager.LoadScene(1);
-        }
+        ani = GetComponent<Animator>();
+        // 정확하게 애니메이션 시작
+        // 애니메이션 위치를 초기화 하고, 애니메이션 이름으로 Play
+        //ani.Rebind();
+        ani.Play("intro");
+        Debug.Log("1");
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
+    // Update is called once per frame
+    void Update()
+    {
+        // 애니메이션 종료는 Coroutine으로 확인하자
+        // 끝났는지 확인(시간이 normalized 되어서 0.0f~1.0f를 가짐)
+        if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            Debug.Log("3");
+            SceneManager.LoadScene(1);
+        }
+        Debug.Log("2");
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    }
 }
